6ef26e90338426d720f485642a39e827b6783f76
Joel Paucarima Franco <joel.dev.3112@gmail.com>
2023-11-26 23:28:32 +0100

    add notification icon (#2)

--- src/main/java/one/util/ideaplugin/screenshoter/CopyImageAction.java
+++ src/main/java/one/util/ideaplugin/screenshoter/CopyImageAction.java
@@ -1,5 +1,7 @@
 package one.util.ideaplugin.screenshoter;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationAction;
 import com.intellij.notification.NotificationGroupManager;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -8,10 +10,21 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.util.SystemProperties;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Tagir Valeev
@@ -19,6 +32,7 @@ import java.awt.datatransfer.Clipboard;
 public class CopyImageAction extends DumbAwareAction {
 
     static final long SIZE_LIMIT_TO_WARN = 3_000_000L;
+    static final DateTimeFormatter DATE_TIME_PATTERN = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
@@ -51,7 +65,56 @@ public class CopyImageAction extends DumbAwareAction {
             NotificationGroupManager.getInstance().getNotificationGroup("Code Screenshots")
                 .createNotification("Image was copied to the clipboard", NotificationType.INFORMATION)
                 .setTitle("Code screenshots")
+                .addAction(NotificationAction.create("Save to File", anActionEvent -> saveImage(image, project)))
                 .notify(editor.getProject());
+        }
+    }
+
+    private void saveImage(@NotNull TransferableImage<?> image, @NotNull Project project) {
+        CopyImageOptionsProvider.State options = CopyImageOptionsProvider.getInstance(project).getState();
+        String toSave = options.myDirectoryToSave;
+        if (StringUtil.isEmpty(toSave)) {
+            toSave = SystemProperties.getUserHome();
+        }
+        toSave = toSave.trim();
+        LocalDateTime now = LocalDateTime.now();
+        String date = DATE_TIME_PATTERN.format(now);
+        String fileName = "Shot_" + date + "." + image.format.ext;
+        Path path = Paths.get(FileUtil.toSystemDependentName(toSave), fileName);
+        try {
+            Files.createDirectories(path.getParent());
+
+            try (OutputStream os = Files.newOutputStream(path)) {
+                image.write(os);
+            }
+
+            String pathRepresentation = StringUtil.escapeXmlEntities(StringUtil.shortenPathWithEllipsis(path.toString(), 50));
+            Notification notification = CopyImagePlugin.getNotificationGroup()
+                    .createNotification(pathRepresentation, NotificationType.INFORMATION)
+                    .setTitle("Code screenshots")
+                    .setSubtitle("Image was saved:");
+            if (Desktop.isDesktopSupported()) {
+                notification.addAction(new NotificationAction("Open") {
+                    @Override
+                    public void actionPerformed(@NotNull AnActionEvent anActionEvent, @NotNull Notification notification) {
+                        try {
+                            Desktop.getDesktop().open(path.toFile());
+                        } catch (IOException e) {
+                            CopyImagePlugin.showError(project, "Cannot open image:  " + StringUtil.escapeXmlEntities(
+                                    path.toString()) + ":<br>" + StringUtil.escapeXmlEntities(
+                                    StringUtil.notNullize(e.getLocalizedMessage())));
+                        }
+                    }
+                });
+            }
+            notification.notify(project);
+        } catch (FileAlreadyExistsException e) {
+            CopyImagePlugin.showError(project, "Cannot save image:  " + StringUtil.escapeXmlEntities(
+                    path.toString()) + ":<br>Not a directory: " + StringUtil.escapeXmlEntities(e.getFile()));
+        } catch (IOException e) {
+            CopyImagePlugin.showError(project, "Cannot save image:  " + StringUtil.escapeXmlEntities(
+                    path.toString()) + ":<br>" + StringUtil.escapeXmlEntities(
+                    StringUtil.notNullize(e.getLocalizedMessage())));
         }
     }
 
