0a07496d42ac1cc98dddf020817184eb0ea2d76a
Joel Paucarima Franco <joel.dev.3112@gmail.com>
2024-04-13 18:00:39 +0200

    bump to v2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
-//    implementation(libs.annotations)
+    implementation("org.apache.xmlgraphics:batik-all:1.17")
 }
 
 // Set the JVM language level used to build the project.
